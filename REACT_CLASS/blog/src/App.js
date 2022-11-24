34320766e04b47e57d292b416e58dc5576d295ba
sh.choi_MAC <>
2022-11-24 22:06:22 +0900

    애플코딩_기초특강 6강까지 완강

--- REACT_CLASS/blog/src/App.js
+++ REACT_CLASS/blog/src/App.js
@@ -13,12 +13,22 @@ function App() {
   let posts = '이정후 짱짱맨';
   let titleStyle =  { color : 'tomato', fontSize : '25px'}
 
+  function updateTitle() {
+    // 1. 일단 기존 state의 deep copy
+    var newArray = [...글제목];
+    // 2. copy한 변수에 수정사항 반영
+    newArray[0] = '날씨 안 추워';
+    // 3. 변경함수()에 집어넣기
+    글제목변경( newArray );
+  }
+
 
   return (
     <div className="App">
       <div className="black-nav">
         <div style={ titleStyle }>개발 Blog</div>
       </div>
+      {/* <button onClick={ updateTitle }>버튼</button> */}
 
       <div className='list'>
         <h3> { 글제목[0] } <span className='likeBtn' onClick={ ()=>{ updateLike( like + 1 ) } }>👍🏻</span> { like } </h3>
@@ -33,10 +43,26 @@ function App() {
       <div className='list'>
         <h3> { 글제목[2] } </h3>
         <p>22.11.24 발행</p>
-        <hr />
+        <hr/>
       </div>
 
+      {/* <Modal></Modal> */}
+      <Modal />
+
+
+
     </div>
+  );
+}
+
+// 새로운 컴포넌트 만들기
+function Modal() {
+  return (
+      <div className='modal'>
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
   );
 }
 
