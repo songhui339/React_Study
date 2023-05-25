7c9536b77551f8225f6279c0023da01796419f6a
shchoi_ptk <csonghui339@gmail.com>
2023-05-25 17:31:34 +0900

    3장 컴포넌트 실습 #1 회사 노트북에서는 맥북에서 작업한 React 프로젝트 파일 실행이 되지 않아 부득이하게 새로운 프로젝트 생성함

---
+++ PTK_REACT_STUDY/hello-react-window/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MyComponent from './MyComponent';
+
+const App = () => {
+  return <MyComponent name="react" favoriteNumber={51}>리액트</MyComponent>
+};
+
+export default App;
