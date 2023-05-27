a0becc496eb4a9999d29297e86ad70e735a4e9f6
shchoi <csonghui339@gmail.com>
2023-05-27 23:42:09 +0900

    📝 230527_스터디 발표 내용 정리

--- PTK_REACT_STUDY/hello-react-window/src/EventPractice.js
+++ PTK_REACT_STUDY/hello-react-window/src/EventPractice.js
@@ -1,6 +1,8 @@
 import React, { Component, useState } from 'react';
 
 // 함수형 컴포넌트로 구현한 이벤트
+// #1 onChange 함수 활용 
+/*
 const EventPractice = () => {
     const [username, setUsername] = useState('');
     const [message, setMessage] = useState('');
@@ -34,6 +36,64 @@ const EventPractice = () => {
                 placeholder="아무거나 입력해 보세요"
                 value={message}
                 onChange={onChangeMessage}
+                onKeyDown={onKeyDown}
+            />
+
+            <button onClick={onClick}>확인</button>
+        </div>
+    );
+};
+*/
+
+// #2 useState 함수 활용
+const EventPractice = () => {
+    const [form, setForm] = useState({
+        username: '',
+        message: ''
+    });
+
+    const { username, message } = form;
+    const onChange = e => {
+        const nextForm = {
+            // 기존의 form 내용을 이 자리에 복사한 뒤
+            ...form,
+            // 원하는 값을 덮어 씌우기
+            [e.target.name]: e.target.value
+        };
+
+        setForm(nextForm);
+    };
+
+    const onClick = () => {
+        alert(username + ': ' + messsage);
+        setForm ({
+            username: '',
+            message: ''
+        });
+    };
+
+    const onKeyDown = e => {
+        if (e.key === 'Enter') {
+            onClick();
+        }
+    };
+
+    return (
+        <div>
+            <h1>이벤트 연습_함수 컴포넌트</h1>
+            <input 
+                type="text" 
+                name="username"
+                placeholder="사용자명"
+                value={username}
+                onChange={onChange}
+            />
+            <input 
+                type="text"
+                name="message"
+                placeholder="아무거나 입력해 보세요"
+                value={message}
+                onChange={onChange}
                 onKeyDown={onKeyDown}
             />
 
